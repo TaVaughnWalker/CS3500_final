f107b612cd8129de61fef5c8c6f9225b5d072cb0
CassidyNorkett <123487502+CassidyNorkett@users.noreply.github.com>
2023-04-19 21:31:40 -0400

    Create genreswitch.php
    changed html format to php, changed background colot, font, and font size

---
+++ views/genreswitch.php
@@ -0,0 +1,46 @@
+  <body>
+ <?php 
+    $bg_color = '#ffcccb';  
+    echo "<body style='background-color: $bg_color;'>"; 
+    echo "<h1> <font color=black font face='gadugi' size='7pt'>From Country to Pop Star</font> </h1>";
+echo  '<img src="https://i.insider.com/638f65a257aefa0018eba098?width=1000&format=jpeg&auto=webp"/>';
+
+
+echo "<h2> <font color=black font face='gadugi' size='7pt'>From The Beginning</font> </h3>";
+
+echo "<p> <font color=black font face='gadugi' size='5pt'>Taylor Swift grew up in West Reading, Pennsylvania, but moved to Tennessee when she was 13 years old so she could pursue a singing career. Because of this move, Taylor started off singing country music, not the hit pop-songs we know from her today.</font> </p>";
+echo "<p> <font color=black font face='gadugi' size='5pt'>Taylor Swift had her first nationwide, headlining tour for her <i>Fearless</i> album in 2009, but before that she went on her first tour as an opener for Rascal Flatts, securing her spot as a country star at age 16. Taylor looked the role with her wispy blonde hair and cowboy boots. Taylor released three country albums. She was solely a country music star until she released her pop album, <i>'Red'</i>, in 2012.
+</font> </p>";
+
+
+echo  '<img src="https://media.glamourmagazine.co.uk/photos/6138d6db2bec5fcec32c418d/16:9/w_1280,c_limit/taylor-swift_glamour_24oct16_pa-b.jpg"  />';
+
+echo "<h2> <font color=black font face='gadugi' size='7pt'>Red</font> </h3>";
+
+echo "<p> <font color=black font face='gadugi' size='5pt'>After dropping her album, fans went into a frenzy. Taylor was no longer a country music star, but making her way into a pop icon, and fast. The messages within her songs still had the same theme, but the genre took a U-Turn. Most fans were pleasantly shocked at this sudden change. However, others did not want Taylor to shy away from country music. Since <i>'Red'</i>, Taylor Swift has come out with eight, and counting, pop albums since 2012. She has found her success in the pop genre.</font> </p>";
+echo "<p> <font color=black font face='gadugi' size='5pt'>Even though she is a pop star, Taylor's fans from her country era are still not left out. Taylor released, <i>'Fearless (Taylor's Version)</i> in 2021 where she released new songs and re-recorded her original songs on this country album.</font> </p>";
+
+
+echo "<h2> <font color=black font face='gadugi' size='7pt'>Why The Change?</font> </h3>";
+
+
+echo "<p> <font color=black font face='gadugi' size='5pt'>After the release of <i>'Red'</i>, a lot of fans were wondering why she chose to switch genres. Taylor Swift, herself, stated that she wanted to experiment with pop. Taylor noted that she wanted to challenge herself in an exploratory way and jumped from country to pop.</font> </p>";
+
+echo "<p> <font color=black font face='gadugi' size='5pt'>Her experiment was successful to say the least. She is one of the most well-known pop stars of the day. If she had not made the switch to pop from country, there is no saying how her career would have turned out, or if her fan base would have grown the way it did. However, she did make the transition to pop... and the rest is history.</font> </p>";
+
+  ?>  
+  </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
