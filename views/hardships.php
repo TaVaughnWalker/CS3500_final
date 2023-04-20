f37a2e7bc1abf37315518f0106608aea8278f0f1
CassidyNorkett <123487502+CassidyNorkett@users.noreply.github.com>
2023-04-19 21:52:39 -0400

    Update hardships.php

--- views/hardships.php
+++ views/hardships.php
@@ -1,35 +1,3 @@
- <body>
- <?php 
-    $bg_color = '#ADD8E6';  
-    echo "<body style='background-color: $bg_color;'>"; 
-    echo "<h1> <font color=black font face='gadugi' size='7pt'>Hardships</font> </h1>";
-
-    echo "<p> <font color=black font face='gadugi' size='7pt'>As a female pop artist, Taylor Swift has had her fair share of hardships in show business. Taylor Swift has faced backlash for the muse of her songwriting, the awards that she has won, and rejection from producers and record companies. The hardships and derailments only seemed to make her stronger as a singer and a woman.</font> </p>";
-echo  '<img src="https://www.rollingstone.com/wp-content/uploads/2009/09/9515192dW.jpg"  />';
-
-
-echo "<h2> <font color=black font face='gadugi' size='7pt'>Kanye West and Taylor Swift: VMAs 2009</font> </h3>";
-echo  '<a href="https://www.sportskeeda.com/pop-culture/what-did-kanye-west-do-to-taylor-swift-at-the-grammys#:~:text=Kanye%20West%20interrupted%20Taylor%20Swift,best%20videos%20of%20all%20time!%20%22"> Kanye West and Taylor Swift VMAs 2009</a>';
-
-echo "<p> <font color=black font face='gadugi' size='5pt'>In 2009, Taylor Swift won a VMA for 'Video of the Year.' When Taylor Swift walked on stage to give her acceptance speech, she was interrupted by Kanye West 
-who took the microphone and stated, 'I'mma let you finish, but Beyoncé had one of the best videos of all time, one of the best videos of all time!'
-his moment was stolen from Taylor Swift and started a feud with West which placed turmoil on her career. However, her fan base, the 'Swifties,' had her back 
-and she had more support than ever.</font> </p>";
-
-echo "<h2> <font color=black font face='gadugi' size='7pt'>Sexist Critiques</font> </h3>";
-echo  '<a href="https://www.bupipedream.com/opinions/124396/taylor-swift-is-a-victim-of-sexist-criticism/"> Sexist Critiques</a>';
-
-echo "<p> <font color=black font face='gadugi' size='5pt'>People seem to think that Taylor Swift only writes music about her exes. This can be disheartening to an artist who knows that is not the truth. Taylor Swift writes about a variety of topics in her songs, yet people criticize her for writing shallow music. Because of this, Taylor Swift is deemed Boy-Crazy just because she often writes about her feelings of current and past relationships. She is a victim of sexist remarks based on how many guys or how many songs that she writes about. This has both made her songwriting career difficult with her haters, but also inspired her to write about the haters as well.</font> </p>";
-
-echo  '<img src="https://media.glamourmagazine.co.uk/photos/6138d6db2bec5fcec32c418d/16:9/w_1280,c_limit/taylor-swift_glamour_24oct16_pa-b.jpg"  />';
-
-
-echo "<h2> <font color=black font face='gadugi' size='7pt'>Rejection</font> </h3>";
-echo  '<a href="https://en.wikipedia.org/wiki/Taylor_Swift_(album)#:~:text=At%20age%20eleven%2C%20Swift%20traveled,girl%2C%20which%20Swift%20firmly%20disbelieved."> Rejection</a>';
-
-echo "<p> <font color=black font face='gadugi' size='5pt'>We all know the huge success that Taylor Swift is. She has ten albums, four EPs, and two re-recordings. However, even a globally known popstar has her fair share of rejection.
-              When Swift was eleven-years-old, she submitted a demo recording of covers to record labels, but was ultimately rejected due to the fact that the record companies believed their 
-              listener demographic would not listen to Taylor. Because of this, Taylor Swift started to develop her own music, learned to play the guitar, and begain writing 
-              her own lyrics in hopes to create her own 'sound.' This paid off for Taylor because at only 14-years-old, she signed on with Sony/ATV Publishing House.</font> </p>";
-  ?>  
-  </body>
+<?php
+require('inc/header.inc');
+include('static/hardships.html');
