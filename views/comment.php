b9ccfd755a49de241886f294f1dabfafc20fcaf2
Jadon <jadonzufall@gmail.com>
2023-04-19 21:44:56 -0400

    Method validation on comments form

--- views/comment.php
+++ views/comment.php
@@ -1,3 +1,12 @@
 <?php
-require('inc/header.inc');
-require('forms/comment_get.php');
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    require('inc/header.inc');
+    require('forms/comment_get.php');
+}
+else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require('inc/header.inc');
+    require('forms/comment_post.php');
+}
+else {
+    echo "Invalid Request Method";
+}
